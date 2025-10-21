7b88e70669ed889af378d43ac72fee76b2dbb81d
jjHernandez17 <juanjohervar1708@gmail.com>
2025-10-21 10:29:23 -0500

    Actu

--- Clim_weather_API.py
+++ Clim_weather_API.py
@@ -2,7 +2,7 @@ import requests;
 
 ciudades = []
 
-
+# Función para obtener coordenadas usando la API de Open-Meteo
 def obtener_coordenadas(ciudad):
     geo_url = "https://geocoding-api.open-meteo.com/v1/search"
 
@@ -17,12 +17,27 @@ def obtener_coordenadas(ciudad):
     else:
         print("No se encontro la ciudad")
         return None, None, None
-
+# Bucle principal para solicitar ciudades y obtener coordenadas
 while True:
     ciudad = input("🌍 Ingrese el nombre de la ciudad: ")
 
     lat, lon, nombre = obtener_coordenadas(ciudad)
 
-
+    # Se verifica que se hayan obtenido latitud y longitud
     if lat and lon:
-        ciudades.append({"Ciudad":nombre, "Latitud": lat, "Longitud": lon})
+        ciudades.append({"Ciudad":nombre, "Latitud": lat, "Longitud": lon}) 
+        seleccion = input("¿Deseas intentar con otra ciudad? (si/no): ").strip().lower()
+        if seleccion == "no":
+            print("Coordenadas registradas:")
+            for c in ciudades:
+                print(c)
+            print("Cerrando programa")
+            break
+    else:
+        print("Intenta con otra ciudad.")
+        continue
+            
+    # constructor final
+    print("Coordenadas registradas:")
+    for c in ciudades:
+        print(c)
